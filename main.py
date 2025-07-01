2f936602b4f3ff5f1799c6620f7caaa69504d4d7
tong thai <44336526-egvthai@users.noreply.replit.com>
2025-07-01 15:11:05 +0000

    Sửa TOKEN để dùng biến môi trường

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
+import os
 from keep_alive import keep_alive
 import telebot
 from googletrans import Translator
 
-# 🔑 Thay bằng token thật của bạn từ BotFather
-TOKEN = '8041101424:AAF1EURxhE7gTpLYnybS2cVzSVO6Ccu5OTA'
+TOKEN = os.getenv("TELEGRAM_TOKEN")  # chỉ cần dòng này
 
 bot = telebot.TeleBot(TOKEN)
 translator = Translator()
+
 
 @bot.message_handler(func=lambda message: True)
 def translate_message(message):
@@ -14,11 +15,14 @@ def translate_message(message):
         src_lang = translator.detect(message.text).lang
         if src_lang != 'vi':
             translated = translator.translate(message.text, dest='vi')
-            bot.reply_to(message, f"🈯 Dịch từ {src_lang.upper()}:\n\n{translated.text}")
+            bot.reply_to(
+                message, f"🈯 Dịch từ {src_lang.upper()}:\n\n{translated.text}")
         else:
             bot.reply_to(message, "✅ Tin nhắn đã là tiếng Việt.")
     except Exception as e:
         bot.reply_to(message, "❌ Có lỗi khi dịch.")
         print("Lỗi:", e)
+
+
 keep_alive()
 bot.polling()
