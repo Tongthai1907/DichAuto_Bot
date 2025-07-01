5b1780de4b247f42f11389219a177b0bc613ecfd
tong thai <44336526-egvthai@users.noreply.replit.com>
2025-07-01 16:33:48 +0000

    Chuyển sang google_trans_new để tránh lỗi cgi

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import os
 from keep_alive import keep_alive
 import telebot
-from googletrans import Translator
+# from googletrans import Translator  ← xoá dòng cũ
+from google_trans_new import google_translator  # thêm dòng mới
 
 TOKEN = os.getenv("TELEGRAM_TOKEN")  # chỉ cần dòng này
 
 bot = telebot.TeleBot(TOKEN)
-translator = Translator()
+# translator = Translator()
+translator = google_translator()
 
 
 @bot.message_handler(func=lambda message: True)
@@ -14,9 +16,10 @@ def translate_message(message):
     try:
         src_lang = translator.detect(message.text).lang
         if src_lang != 'vi':
-            translated = translator.translate(message.text, dest='vi')
-            bot.reply_to(
-                message, f"🈯 Dịch từ {src_lang.upper()}:\n\n{translated.text}")
+            translated = translator.translate(message.text, lang_tgt='vi')
+            bot.reply_to(message,
+                         f"📗 Dịch từ {src_lang.upper()}:\n\n{translated}")
+
         else:
             bot.reply_to(message, "✅ Tin nhắn đã là tiếng Việt.")
     except Exception as e:
