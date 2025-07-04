81edf58c05bf8a5ee93d8df3640abb7485b86af4
Tongthai1907 <eg.vthai@gmail.com>
2025-07-05 01:05:54 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 import os
-from keep_alive import keep_alive
 import telebot
 from deep_translator import GoogleTranslator
+from keep_alive import keep_alive
 
-TOKEN = os.getenv("TELEGRAM_TOKEN")  # Đặt biến môi trường trong Render
-
+# Đặt TELEGRAM_TOKEN trong môi trường Render
+TOKEN = os.getenv("TELEGRAM_TOKEN")
 bot = telebot.TeleBot(TOKEN)
-
 
 @bot.message_handler(func=lambda message: True)
 def translate_message(message):
     try:
         text = message.text
-        translated = GoogleTranslator(source='auto',
-                                      target='vi').translate(text)
-        bot.reply_to(message, f"📥 Đã dịch:\n\n{translated}")
+        translated = GoogleTranslator(source='auto', target='vi').translate(text)
+        bot.reply_to(message, f"📤 Đã dịch:\n\n{translated}")
     except Exception as e:
         bot.reply_to(message, "❌ Có lỗi khi dịch.")
         print("Lỗi:", e)
 
-
+# Gọi Flask server để giữ cho bot sống khi dùng Render + UptimeRobot
 keep_alive()
+
+# Bắt đầu nhận tin nhắn từ Telegram
 bot.polling()
